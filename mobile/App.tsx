6955419c3763052091caf7d81516756c514c2332
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-07-01 20:36:13 -0300

    Componente Loading.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { 
   useFonts, 
   Inter_400Regular, 
@@ -7,6 +6,8 @@ import {
   Inter_700Bold, 
   Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
+
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,13 +18,13 @@ export default function App() {
   })
 
   if (!fontsLoaded) {
-    return
+    return <Loading />
   }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </View>
   );
 }
@@ -31,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
