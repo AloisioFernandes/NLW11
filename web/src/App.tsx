5ef5f210ea84c393ae31ede53fee26638cced292
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-07-10 20:05:43 -0300

    Início front end.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,19 @@
 import './styles/global.css'
 
-function App() {
+import logoImage from "./assets/logo.svg"
+
+export function App() {
   return (
-    <h1>Root</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoImage} alt="habits" />
+
+          <button type="button">
+            
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default App
