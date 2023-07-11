43ba48029266c89eba91e382486d02121483b43d
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-07-11 20:35:28 -0300

    Componente Header.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,11 @@
 import './styles/global.css'
-
-import logoImage from "./assets/logo.svg"
+import { Header } from './components/Header'
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
-        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
-          <img src={logoImage} alt="habits" />
-
-          <button type="button">
-            
-          </button>
-        </div>
+        <Header />
       </div>
     </div>
   )
