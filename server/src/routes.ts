b0396a191632ced2264fce9b2731c8f8e69874e6
AloisioFernandes <aloisio.fernandes@outlook.com>
2023-07-31 22:24:56 -0300

    Query.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -131,7 +131,12 @@ export async function appRoutes(app: FastifyInstance) {
             cast(count(*) as float)
           FROM day_habits DH
           WHERE DH.day_id = D.id
-        ) as completed
+        ) as completed,
+        (
+          SELECT
+          cast(count(*) as float)
+          FROM habit_week_days HWD
+        ) as amount
       FROM days D
     `
 
