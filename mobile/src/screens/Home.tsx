1ab0f6c05ee08929ad5f3af14a1f0a5fa5a37556
AloisioFernandes <aloisio.fernandes@outlook.com>
2024-10-13 11:54:30 -0300

    Porcentagens web.

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -55,3 +55,4 @@ export function Home() {
     </View>
   )
 }
+// 1:25:45
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,8 +5,8 @@ const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = generateDatesFromYearBeginning();
 
-const minimumSummaryDatesSize = 18 * 7
-const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+const minimumSummaryDatesSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -26,13 +26,23 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />;
-        })}
-        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => {
           return (
-            <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
-          )
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          );
         })}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
   );
