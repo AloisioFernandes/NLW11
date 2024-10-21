543c9b366b3305d496cd90b246a558472c3a1f64
AloisioFernandes <aloisio.fernandes@outlook.com>
2024-10-21 07:09:57 -0300

    Buscando resumo do backend.

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -3,6 +3,8 @@ import {
   Dimensions,
   TouchableOpacityProps,
 } from "react-native";
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
+import clsx from "clsx";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
@@ -11,12 +13,27 @@ export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
   Dimensions.get("screen").width / WEEK_DAYS - (SCREEN_HORIZONTAL_PADDING + 5);
 
-interface Props extends TouchableOpacityProps {}
+interface Props extends TouchableOpacityProps {
+  amountOfHabits?: number;
+  amountCompleted?: number;
+  date: Date;
+}
 
-export function HabitDay({ ...rest }: Props) {
+export function HabitDay({
+  amountOfHabits = 0,
+  amountCompleted = 0,
+  date,
+  ...rest
+}: Props) {
+  const amountAccomplishedPercentage =
+    amountOfHabits > 0
+      ? generateProgressPercentage(amountOfHabits, amountCompleted)
+      : 0;
   return (
     <TouchableOpacity
-      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      className={clsx("rounded-lg border-2 m-1", {
+        ["bg-zinc-900 border-zinc-800"]: amountAccomplishedPercentage === 0,
+      })}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.7}
       {...rest}
